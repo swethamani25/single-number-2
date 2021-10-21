7801486caf9fb40ad0d961c5980c93f733351731
swethamani25 <92358649+swethamani25@users.noreply.github.com>
2021-10-21 11:37:31 +0530

    Add files via upload

---
+++ b3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ones, twos = 0, 0
+        for n in nums:
+            ones = (ones ^ n) & ~twos
+            twos = (twos ^ n) & ~ones
+
+        return ones
+        
